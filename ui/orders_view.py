68ba40b21c6b8db3c6f7e6b713cb3f1173a9b6cb
Igor Konovalenko <skaylic@yandex.ru>
2025-12-11 20:32:43 +1100

    Create orders_view.py

---
+++ ui/orders_view.py
@@ -0,0 +1,14 @@
+import flet as ft
+
+
+class OrdersView(ft.Column):
+    def __init__(self, okx_client):
+        super().__init__()
+        self.okx_client = okx_client
+        self.expand = True
+
+        self.controls = [
+            ft.Text("Ордера", size=24, weight=ft.FontWeight.BOLD),
+            ft.Text("Здесь будут активные и исторические ордера"),
+            # Добавьте таблицу с ордерами
+        ]
